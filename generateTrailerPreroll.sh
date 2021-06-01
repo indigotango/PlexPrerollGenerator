f39e48b1fdce386624e9c383a58f36490346ca7e
indigotango <indigotango@users.noreply.github.com>
2021-05-31 21:15:09 -0800

    Quick customization refinements
    Inserted variable to allow for easy customization of STEP 1 alpha channel file; inserted variables to simply/clean up STEP 2 font file designations.

--- generateTrailerPreroll.sh
+++ generateTrailerPreroll.sh
@@ -7,10 +7,12 @@
 ## seek to 10 seconds in, trim to 12 seconds after that;
 ## use FFMPEG lossless codec (ffv1) to preserve quality
 
-# Specify path to movie trailer video file
+# Specify file paths for movie trailer + card overlay ("alpha channel")
 plexgen_TrailerFile=<trailerVideo.file>
+plexgen_Alpha=alpha_1080.mov
+#plexgen_Alpha=alpha_1080_loader.mov
 
-ffmpeg -ss 10 -i $plexgen_TrailerFile -i alpha_1080.mov -to 12 \
+ffmpeg -ss 10 -i $plexgen_TrailerFile -i $plexgen_Alpha -to 12 \
 -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
 -vcodec ffv1 -an out1.mkv
 
@@ -20,17 +22,22 @@ ffmpeg -ss 10 -i $plexgen_TrailerFile -i alpha_1080.mov -to 12 \
 plexgen_NowShowing="Now Showing on Plex"
 plexgen_Title="Sample Movie Trailer"
 
+# Specify file paths for fonts
+plexgen_FontNowPlaying=arial.ttf
+plexgen_FontTitle=arial_bold.ttf
+plexgen_FontDescription=arial_italic.ttf
+
 # Perform text overlay in FFMPEG
 ffmpeg -i out1.mkv -filter_complex "[0:v]\
-  drawtext=fontfile=arial.ttf:\
+  drawtext=fontfile=$plexgen_FontNowPlaying.ttf:\
   text='$plexgen_NowShowing':\
   fontsize=68:fontcolor=e5a00d:\
   alpha='if(lt(t,2.25),0,if(lt(t,3),(t-2.25)/0.5,if(lt(t,12),1,if(lt(t,13),(1-(t-12))/1,0))))':x=145:y=400, \
-  drawtext=fontfile=arial_bold.ttf:\
+  drawtext=fontfile=$plexgen_FontTitle.ttf:\
   text='$plexgen_Title':\
   fontsize=120:fontcolor=ffffff:\
   alpha='if(lt(t,2.35),0,if(lt(t,3),(t-2.35)/0.5,if(lt(t,12),1,if(lt(t,13),(1-(t-12))/1,0))))':x=142:y=495, \
-  drawtext=fontfile=arial_italic.ttf:\
+  drawtext=fontfile=$plexgen_FontDescription.ttf:\
   textfile=text_MovieDescription.txt:\
   fontsize=40:fontcolor=e5a00d:\
   line_spacing=11:\
