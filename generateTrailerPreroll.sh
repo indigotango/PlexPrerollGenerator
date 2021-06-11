e481219be16128cd6170f27af09aa92abf539fda
indigotango <indigotango@users.noreply.github.com>
2021-06-10 22:33:31 -0800

    Inserted FFMPEG step for shifted trailer center
    This change inserts a step before the alpha channel overlay step to shift the trailer's center-point over 50%, and then use a smooth wipe to reveal it over the original trailer frames. This allows the trailer to be better presented underneath the alpha channel overlay (the "cards"). KNOWN ISSUE: the offset-overlay has a bug where the offset trailer overlaid over the original does not sync up; I suspect this lies with the overlay filter starting the video frames from the 10-second point as specified with the input file options at the start of the FFMPEG command. Need to fix.

--- generateTrailerPreroll.sh
+++ generateTrailerPreroll.sh
@@ -2,51 +2,51 @@
 ## Generate Plex prerolls to highlight recent movie
 ## additions to a Plex Media Server instance.
 
-## STEP 1: alpha overlay on trailer, output as lossless (ffv1)
-## SOURCE: https://superuser.com/a/1520509
+# Specify path to movie trailer video file
+plexgen_TrailerFile=TENET.mp4
+
+## STEP 1: generate trimmed + shifted trailer
+## SOURCE: https://superuser.com/a/727388
 ## seek to 10 seconds in, trim to 12 seconds after that;
 ## use FFMPEG lossless codec (ffv1) to preserve quality
+ffmpeg -ss 10 -i $plexgen_TrailerFile -to 12 -filter_complex "\
+  [0:v][0:v]overlay=W/2:0[bg]; \
+  [bg] [0:v]overlay=W/2-W[shifted]; \
+  [0:v][shifted]xfade=transition=smoothleft:duration=1:offset=1.75[out]" \
+-map "[out]" -vcodec ffv1 -an out1.mkv
 
-# Specify file paths for movie trailer + card overlay ("alpha channel")
-plexgen_TrailerFile=<trailerVideo.file>
-plexgen_Alpha=alpha_1080.mov
-#plexgen_Alpha=alpha_1080_loader.mov
-
-ffmpeg -ss 10 -i $plexgen_TrailerFile -i $plexgen_Alpha -to 12 \
+## STEP 2: alpha overlay on trailer, output as lossless (ffv1)
+## SOURCE: https://superuser.com/a/1520509
+ffmpeg -i out1.mkv -i alpha_1080.mov -to 12 \
 -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
--vcodec ffv1 -an out1.mkv
+-vcodec ffv1 -an out2.mkv
 
-## STEP 2: overlay Coming Soon / Title / Description text
+## STEP 3: overlay Coming Soon / Title / Description text
 ## SOURCE: http://ffmpeg.shanewhite.co/
 # Text for FFMPEG to overlay
 plexgen_NowShowing="Now Showing on Plex"
 plexgen_Title="Sample Movie Trailer"
 
-# Specify file paths for fonts
-plexgen_FontNowPlaying=arial.ttf
-plexgen_FontTitle=arial_bold.ttf
-plexgen_FontDescription=arial_italic.ttf
-
 # Perform text overlay in FFMPEG
-ffmpeg -i out1.mkv -filter_complex "[0:v]\
-  drawtext=fontfile=$plexgen_FontNowPlaying.ttf:\
+ffmpeg -i out2.mkv -filter_complex "[0:v]\
+  drawtext=fontfile=arial.ttf:\
   text='$plexgen_NowShowing':\
   fontsize=68:fontcolor=e5a00d:\
   alpha='if(lt(t,2.25),0,if(lt(t,3),(t-2.25)/0.5,if(lt(t,12),1,if(lt(t,13),(1-(t-12))/1,0))))':x=145:y=400, \
-  drawtext=fontfile=$plexgen_FontTitle.ttf:\
+  drawtext=fontfile=arial_bold.ttf:\
   text='$plexgen_Title':\
   fontsize=120:fontcolor=ffffff:\
   alpha='if(lt(t,2.35),0,if(lt(t,3),(t-2.35)/0.5,if(lt(t,12),1,if(lt(t,13),(1-(t-12))/1,0))))':x=142:y=495, \
-  drawtext=fontfile=$plexgen_FontDescription.ttf:\
+  drawtext=fontfile=arial_italic.ttf:\
   textfile=text_MovieDescription.txt:\
   fontsize=40:fontcolor=e5a00d:\
   line_spacing=11:\
   alpha='if(lt(t,2.45),0,if(lt(t,3),(t-2.45)/0.5,if(lt(t,12),1,if(lt(t,13),(1-(t-12))/1,0))))':x=145:y=640" \
--vcodec ffv1 out2.mkv
+-vcodec ffv1 out3.mkv
 
-## STEP 3: overlay end wipe + music on STEP 2 output,
+## STEP 4: overlay end wipe + music w/ output of STEP 3,
 ## use FFMPEG .mp4 encoding defaults (H.264 + AAC)
-ffmpeg -i out2.mkv \
+ffmpeg -i out3.mkv \
 -i endwipe_1080.mov -i BG-music.wav \
 -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
 -crf 18 -tune film preroll_NowShowing.mp4
@@ -54,3 +54,4 @@ ffmpeg -i out2.mkv \
 ## STEP 4: clean up temporary files
 #rm out1.mkv
 #rm out2.mkv
+#rm out3.mkv
